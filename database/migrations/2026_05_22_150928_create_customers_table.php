cec66d58d7235a9c8fff0f3ce83fee534f067cad
Moshiur Rahman <m.sujon4u@gmail.com>
2026-06-07 23:25:59 +0600

    Update customer  Migration file

--- database/migrations/2026_05_22_150928_create_customers_table.php
+++ database/migrations/2026_05_22_150928_create_customers_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('customers', function (Blueprint $table) {
             $table->id();
+            $table->string('customer_name');
+            $table->string('customer_phone');
+            $table->string('customer_email');
+            $table->string('customer_address');
             $table->timestamps();
         });
     }
